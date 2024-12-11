9e0af9823ab470c9edbe5f808c128ad536c82242
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-12-11 19:14:26 +1100

    add themes

---
+++ nord/index.ts
@@ -0,0 +1,13 @@
+import { Theme } from 'relagit:themes';
+
+export default new Theme({
+	name: 'Nord',
+	authors: [
+		{
+			name: 'TheCommieAxolotl',
+			url: 'https://github.com/TheCommieAxolotl'
+		}
+	],
+	description: 'An arctic, north-bluish color palette.',
+	main: 'index.css'
+});
---
+++ tokyo-night/index.ts
@@ -0,0 +1,13 @@
+import { Theme } from 'relagit:themes';
+
+export default new Theme({
+	name: 'Tokyo Night',
+	authors: [
+		{
+			name: 'TheCommieAxolotl',
+			url: 'https://github.com/TheCommieAxolotl'
+		}
+	],
+	description: 'A clean, dark theme that celebrates the lights of Downtown Tokyo at night.',
+	main: 'index.css'
+});
